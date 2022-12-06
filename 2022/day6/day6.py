d330fb3a0dc54f309d19359615f70c22cc9736b3
Atle Heigre <atle@heigre.xyz>
2022-12-06 12:41:35 +0100

    added minimal solution (stolen partly from Reddit)

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -17,3 +17,8 @@ while True:
             print(f'-> Part 2: {i + som}')
             break
     break
+
+# - - - - - - -
+
+# If I had been smart enough I would have made it like this:
+print(*map(lambda l: [i + l for i in range(len(data)) if len(set(data[i:i + l])) == l][0], [4, 14]))
