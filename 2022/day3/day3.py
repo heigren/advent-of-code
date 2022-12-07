f93406f711736fca322140b8fe75024dc3040918
Atle Heigre <atle@heigre.xyz>
2022-12-07 13:11:10 +0100

    deleted some folders

--- 2022/day3/day3.py
+++
@@ -1,9 +0,0 @@
-import string
-
-# Part 1
-print(sum([dict(zip(string.ascii_letters, [l for l in range(1,53)])).get(''.join(set(y[0]).intersection(y[1]))) for y in [[x[i:i + round(len(x) / 2)] for i in range(0, len(x), round(len(x) / 2))] for x in open('input.txt').read().split('\n') if x]]))
-
-# ---
-
-# Part 2
-print(sum([dict(zip(string.ascii_letters, [l for l in range(1,53)])).get(''.join(set(y[0]).intersection(*y))) for y in list(zip(*[iter([x for x in open('input.txt').read().split('\n') if x])]*3))]))
